2c55fe5affbdd26cd134d82de82e395790f96013
shenwenjie <undefined>
2023-02-10 23:36:29 +0800

    Update index.vue

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,6 +3,9 @@
     <el-button type="primary" @click="toDemo1">大屏demo1</el-button>
     <el-button type="primary" @click="toDemo2">大屏demo2</el-button>
     <el-button type="primary" @click="toDemo3">大屏demo3</el-button>
+    <el-button type="primary" @click="toDemo4">大屏demo4</el-button>
+
+    <p>demo仅做展示使用。样式暂未做自适应无滚动条处理</p>
   </div>
 </template>
 
@@ -23,6 +26,9 @@ export default {
       this.$router.push({
         path: '/demo3'
       })
+    },
+    toDemo4() {
+      window.open('http://121.5.172.29:8080')
     }
   }
 }
